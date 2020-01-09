324264e99a622887dac1434f73a8af12dbd154d9
Ojas Sinha <sinhaojas67@gmail.com>
2020-01-09 08:51:42 +0000

    santoni: Import permissions for QPerformance & UxPerformance
    Signed-off-by: Pranaya Deomani <deomanipranaya007@gmail.com>

--- santoni/santoni-vendor.mk
+++ santoni/santoni-vendor.mk
@@ -2282,4 +2282,6 @@ PRODUCT_PACKAGES += \
     uimremotesimlocklibrary \
     vendor.qti.ims.callinfo-V1.0-java \
     vendor.qti.ims.rcsconfig-V1.0-java \
-    vendor.qti.hardware.sensorscalibrate-V1.0-java
+    vendor.qti.hardware.sensorscalibrate-V1.0-java \
+    com.qualcomm.qti.UxPerformance.xml \
+    com.qualcomm.qti.Performance.xml
